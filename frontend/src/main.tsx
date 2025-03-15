ca19648015dcd3019ad19a7279f23d8aeeb4f99b
gandolh <29356778+gandolh@users.noreply.github.com>
2025-03-15 14:33:31 +0200

    working upload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, redirect, RouterProvider } from 'react-router'
 import './index.css'
 import { Home } from './modules/home/components/Home.tsx'
 import Dashboard from './modules/home/components/Dashboard.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 
 const router = createBrowserRouter([
@@ -32,8 +33,15 @@ const router = createBrowserRouter([
   }
 ])
 
+
+const queryClient = new QueryClient()
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
